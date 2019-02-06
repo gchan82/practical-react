5649fdfec4c4fc8b5402ce8b172120c973ccbd23
gchan82 <42255277+gchan82@users.noreply.github.com>
2019-02-06 06:47:48 -0800

    3 - Create React Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const Body = (props) => (<div>
+  <p className="App-intro">{props.text}</p>
+  <p className="App-intro">{props.text2}</p>
+  <p className="App-intro">{props.myFunc(1,2)}</p>
+</div>);
+
+
+class Header extends Component {
+  render(){
+    return (
+<header className="App-header">
+            <h1 className="App-title">{this.props.title}</h1>
+            {this.props.num}
+            <div>
+            {JSON.stringify(this.props.myObj)}
+            {this.props.myArr[0]}
+            {this.props.myFunc(10,12)}
+            </div>
+        </header>
+    );
+  }
+}
+
 class App extends Component {
+  add(a,b) {
+    return a+b;
+  }
   render() {
+    const add = (a,b) => a+b;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <Header title="Hello" num={4} myObj={{a:5,b: 6}} myArr={[10,11,12]}
+            myFunc={this.add}
+            ></Header>
+            <Body myFunc={this.add} text="i am cool" text2="I am cool2"></Body>
           </p>
           <a
             className="App-link"
